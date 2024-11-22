bbdb4d515ecd8540cd546c2e8bf10c23ff11fc33
SeonJeong <sjkim5621@naver.com>
2024-11-22 16:32:45 +0900

    Feat: [Compose Tutorial] 강의4_Lists and Animations
    - LazyColumn으로 메세지 리스트 생성
    - 메세지 확장, 클릭시 배경색 애니메이션 적용

--- Compose Tutorial/app/src/main/java/com/example/composetutorial/MainActivity.kt
+++ Compose Tutorial/app/src/main/java/com/example/composetutorial/MainActivity.kt
@@ -1,24 +1,32 @@
 package com.example.composetutorial
 
-import android.content.res.Configuration
+import SampleData
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.animateContentSize
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.painterResource
@@ -31,9 +39,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             ComposeTutorialTheme {
-                Surface(modifier = Modifier.fillMaxSize()) {
-                    MessageCard(Message("Android", "Jetpack Compose"))
-                }
+                Conversation(SampleData.conversationSample)
             }
         }
     }
@@ -41,6 +47,7 @@ class MainActivity : ComponentActivity() {
 
 data class Message(val author: String, val body: String)
 
+// 화면 레이아웃 설정
 @Composable
 fun MessageCard(msg: Message) {
     // padding 설정
@@ -56,7 +63,16 @@ fun MessageCard(msg: Message) {
         // 수평 간격 주기
         Spacer(modifier = Modifier.width(8.dp))
 
-        Column {
+        // 확장 상태 추적 변수
+        var isExpanded by remember { mutableStateOf(false) }
+
+        // Surface 색상 설정
+        val surfaceColor by animateColorAsState(
+            if (isExpanded) MaterialTheme.colorScheme.primary else MaterialTheme.colorScheme.surface
+        )
+
+        // Column 클릭 시 상태 변경
+        Column(modifier = Modifier.clickable { isExpanded = !isExpanded }) {
             Text(
                 text = msg.author,
                 color = MaterialTheme.colorScheme.secondary,
@@ -65,31 +81,41 @@ fun MessageCard(msg: Message) {
             // 수직 간격 주기
             Spacer(modifier = Modifier.height(4.dp))
 
-            Surface(shape = MaterialTheme.shapes.medium, shadowElevation = 1.dp) {
+            Surface(
+                shape = MaterialTheme.shapes.medium,
+                shadowElevation = 1.dp,
+                // 배경 색상 점진적으로 변경 primary -> surface
+                color = surfaceColor,
+                // Surface 내부 Text의 maxLines가 변하면 높이를 부드럽게 변경 (padding 크기는 고정)
+                modifier = Modifier.animateContentSize().padding(1.dp)
+            ) {
                 Text(
                     text = msg.body,
                     modifier = Modifier.padding(all = 4.dp),
+                    // 확장된 상태면 전체 보여주기, 아니면 첫줄만 보여주기
+                    maxLines = if (isExpanded) Int.MAX_VALUE else 1,
                     style = MaterialTheme.typography.bodyMedium
                 )
             }
-
         }
     }
 }
 
-@Preview(name = "Light Mode")
-@Preview(
-    uiMode = Configuration.UI_MODE_NIGHT_YES,
-    showBackground = true,
-    name = "Dark Mode"
-)
+// MessageCard에 들어갈 내용 설정
+@Composable
+fun Conversation(messages: List<Message>) {
+    LazyColumn {
+        items(messages) { message ->
+            MessageCard(message)
+        }
+    }
+}
+
+
+@Preview(showBackground = true)
 @Composable
 fun PreviewMessageCard() {
     ComposeTutorialTheme {
-        Surface {
-            MessageCard(
-                msg = Message("Lexi", "Hey, take a look at Jetpack Compose, it's great!")
-            )
-        }
+        Conversation(SampleData.conversationSample)
     }
 }
