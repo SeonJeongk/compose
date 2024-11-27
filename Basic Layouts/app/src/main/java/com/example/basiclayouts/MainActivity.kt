035700679da8cfeba63741d3e9d1d4cfc772d81a
SeonJeong <sjkim5621@naver.com>
2024-11-27 19:53:58 +0900

    Feat: [Basic Layouts] 11. 하단 탐색 - Material
    - NavigationBar 사용

--- Basic Layouts/app/src/main/java/com/example/basiclayouts/MainActivity.kt
+++ Basic Layouts/app/src/main/java/com/example/basiclayouts/MainActivity.kt
@@ -28,9 +28,13 @@ import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountCircle
+import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
@@ -48,13 +52,13 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.example.basiclayouts.ui.theme.BasicLayoutsTheme
+import com.example.basiclayouts.ui.theme.MySootheTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            BasicLayoutsTheme {
+            MySootheTheme {
                 HomeScreen(Modifier.fillMaxSize())
             }
         }
@@ -84,9 +88,10 @@ private val favoriteCollectionsData = listOf(
     DrawableStringPair(R.drawable.fc6_nightly_wind_down, R.string.fc6_nightly_wind_down)
 )
 
+// 화면
 @Composable
 fun HomeScreen(modifier: Modifier = Modifier) {
-    BasicLayoutsTheme {
+    MySootheTheme {
         Column(modifier.verticalScroll(rememberScrollState())) {
             Spacer(Modifier.height(16.dp))
             SearchBar(Modifier.padding(bottom = 8.dp))
@@ -119,6 +124,43 @@ fun HomeSection(
         content()
     }
 }
+
+// 바텀 네비
+@Composable
+private fun SootheBottomNavigation(modifier: Modifier = Modifier) {
+    NavigationBar(
+        containerColor = MaterialTheme.colorScheme.surfaceVariant,
+        modifier = modifier
+    ) {
+        NavigationBarItem(
+            selected = true,
+            onClick = {},
+            icon = {
+                Icon(
+                    imageVector = Icons.Default.Home,
+                    contentDescription = null
+                )
+            },
+            label = {
+                Text(stringResource(R.string.bottom_navigation_home))
+            }
+        )
+        NavigationBarItem(
+            selected = false,
+            onClick = {},
+            icon = {
+                Icon(
+                    imageVector = Icons.Default.AccountCircle,
+                    contentDescription = null
+                )
+            },
+            label = {
+                Text(stringResource(id = R.string.bottom_navigation_profile))
+            }
+        )
+    }
+}
+
 
 // 상단 검색창
 @Composable
@@ -220,23 +262,6 @@ fun FavoriteCollectionCard(
     }
 }
 
-@Preview(showBackground = true, backgroundColor = 0xFFF5F0EE, heightDp = 180)
-@Composable
-fun ScreenContentPreview() {
-    BasicLayoutsTheme { HomeScreen() }
-}
-
-@Preview(showBackground = true, backgroundColor = 0xFFF5F0EE)
-@Composable
-fun HomeSectionPreview() {
-    BasicLayoutsTheme {
-        HomeSection(title = R.string.align_your_body) {
-            AlignYourBodyRow()
-        }
-    }
-}
-
-
 @Composable
 fun FavoriteCollectionGrid(
     modifier: Modifier = Modifier,
@@ -256,8 +281,30 @@ fun FavoriteCollectionGrid(
 
 @Preview(showBackground = true)
 @Composable
+fun BottomNavigationPreview() {
+    MySootheTheme { SootheBottomNavigation() }
+}
+
+@Preview(showBackground = true, backgroundColor = 0xFFF5F0EE, heightDp = 180)
+@Composable
+fun ScreenContentPreview() {
+    MySootheTheme { HomeScreen() }
+}
+
+@Preview(showBackground = true, backgroundColor = 0xFFF5F0EE)
+@Composable
+fun HomeSectionPreview() {
+    MySootheTheme {
+        HomeSection(title = R.string.align_your_body) {
+            AlignYourBodyRow()
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
 fun SearchBarPreview() {
-    BasicLayoutsTheme {
+    MySootheTheme {
         SearchBar(modifier = Modifier)
     }
 }
@@ -265,7 +312,7 @@ fun SearchBarPreview() {
 @Preview(showBackground = true, backgroundColor = 0xFFF5F0EE)
 @Composable
 fun AlignYourBodyElementPreview() {
-    BasicLayoutsTheme {
+    MySootheTheme {
         AlignYourBodyElement(
             drawable = R.drawable.ab1_inversions,
             text = R.string.ab1_inversions,
@@ -277,7 +324,7 @@ fun AlignYourBodyElementPreview() {
 @Preview(showBackground = true)
 @Composable
 fun AlignYourBodyRowPreview() {
-    BasicLayoutsTheme {
+    MySootheTheme {
         AlignYourBodyRow(modifier = Modifier.padding(8.dp))
     }
 }
@@ -285,7 +332,7 @@ fun AlignYourBodyRowPreview() {
 @Preview(showBackground = true, backgroundColor = 0xFFF5F0EE)
 @Composable
 fun FavoriteCollectionCardPreview() {
-    BasicLayoutsTheme {
+    MySootheTheme {
         FavoriteCollectionCard(
             text = R.string.fc2_nature_meditations,
             drawable = R.drawable.fc2_nature_meditations,
@@ -297,7 +344,7 @@ fun FavoriteCollectionCardPreview() {
 @Preview(showBackground = true, backgroundColor = 0xFFF5F0EE)
 @Composable
 fun FavoriteCollectionGridPreview() {
-    BasicLayoutsTheme {
+    MySootheTheme {
         FavoriteCollectionGrid(
             modifier = Modifier.padding(8.dp)
         )
