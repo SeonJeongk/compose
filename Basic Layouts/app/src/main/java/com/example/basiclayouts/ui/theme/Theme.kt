663dcabf5e1860794e97005f78017fa567266d13
SeonJeong <sjkim5621@naver.com>
2024-11-27 19:53:02 +0900

    Feat: [Basic Layouts] 테마 이름 수정

--- Basic Layouts/app/src/main/java/com/example/basiclayouts/ui/theme/Theme.kt
+++ Basic Layouts/app/src/main/java/com/example/basiclayouts/ui/theme/Theme.kt
@@ -38,7 +38,7 @@ private val LightColorScheme = lightColorScheme(
 )
 
 @Composable
-fun BasicLayoutsTheme(
+fun MySootheTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     // Dynamic color is available on Android 12+
     dynamicColor: Boolean = true,
