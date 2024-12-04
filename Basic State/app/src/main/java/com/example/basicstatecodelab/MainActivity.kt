924a85f59be500aaefe533169eb040299f10ae03
SeonJeong <sjkim5621@naver.com>
2024-12-04 20:18:27 +0900

    Feat: [Basic State] 3. Compose에서의 상태
    - Wellness 앱 만들기

--- Basic State/app/src/main/java/com/example/basicstatecodelab/MainActivity.kt
+++ Basic State/app/src/main/java/com/example/basicstatecodelab/MainActivity.kt
@@ -4,12 +4,14 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.basicstatecodelab.ui.theme.BasicStateCodelabTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,30 +19,37 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             BasicStateCodelabTheme {
-                // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    WellnessScreen()
                 }
             }
         }
     }
 }
 
+// 기본 화면
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
+fun WellnessScreen(modifier: Modifier = Modifier) {
+    WaterCounter(modifier)
+}
+
+// 하루동안 마신 물잔 개수
+@Composable
+fun WaterCounter(modifier: Modifier = Modifier) {
+    val count = 0
     Text(
-        text = "Hello $name!",
-        modifier = modifier
+        text = "You've had $count glasses.",
+        modifier = modifier.padding(16.dp)
     )
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun WellnessScreenPreview() {
     BasicStateCodelabTheme {
-        Greeting("Android")
+        WellnessScreen()
     }
 }
